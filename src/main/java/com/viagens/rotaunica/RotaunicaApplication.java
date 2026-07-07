12f137a7661c5e183f34b4f1395746a415659c75
henrynascimentosilva <henrynascimentosilva@gmail.com>
2026-07-07 18:16:01 -0300

    Initial commit

---
+++ src/main/java/com/viagens/rotaunica/RotaunicaApplication.java
@@ -0,0 +1,13 @@
+package com.viagens.rotaunica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaunicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RotaunicaApplication.class, args);
+    }
+
+}
