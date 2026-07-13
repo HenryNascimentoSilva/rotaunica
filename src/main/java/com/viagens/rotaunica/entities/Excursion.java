ae0877286630e98197ea2d4b47707a96dcc98e41
henrynascimentosilva <henrynascimentosilva@gmail.com>
2026-07-13 20:38:06 -0300

    refactor: changing uuid generation to conventional methods

--- src/main/java/com/viagens/rotaunica/entities/Excursion.java
+++ src/main/java/com/viagens/rotaunica/entities/Excursion.java
@@ -1,20 +1,25 @@
 package com.viagens.rotaunica.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Excursion {
 
     @Id
-    @UuidGenerator
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String title;
